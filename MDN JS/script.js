906b9a58a9d3d831ff89f715fb01b0b558d0aa3c
Sahil_Kulkarni <sahil.rordev@gmail.com>
2020-12-11 01:02:15 +0530

    Added code and comments to explain concepts in variable scope and hoisting

--- MDN JS/script.js
+++ MDN JS/script.js
@@ -20,7 +20,10 @@ let new_obj = {
 let {bar} = new_obj;
 console.log(bar);
 
-//Variable hoisting
+
+
+//********************** Variable hoisting *************************
+
 //Though var and let can be declared without a value assignment, they differ when it comes to variable hoisting.
 //For eg: variables using var can be defined after they are called, where as in let, variables have to be defined before being called since they throw an uncaught reference error.
 console.log(b);
@@ -43,13 +46,17 @@ console.log(2+num);
 var null_num = null;
 console.log(null_num);
 
-//Variable scoping
+
+
+//******************** Variable scoping **************************
+
 //Variable declared using var
 var a = 20;
 if(true){
   var a = 10;
 }
 console.log(a);
+//Output is 10
 
 //Variable declared using let & const
 let a = 20;
@@ -57,26 +64,17 @@ if(true){
   let a = 10;
 }
 console.log(a);
+//Output is 20
 
+//In the above example we can notice the difference between declaring variables using let and var. Since let is a block scoped variable, the variable is redeclared inside the block as a new variable and the value assigned to it (value is 10) remains only as long as it is in the scope. It then returns back to the original value(20) as soon as it exits the block. Since var is function scoped, it is not affected by block statements and remains to be a global variable as long as it is outside a function block.
 
+var a = 20;
+function one(){
+  var a = 10;
+}
+one();
+console.log(a);
+//20
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//In the above example, both let and var behave the same, since the variable "a" with the value 10 is local only to the function block.
+//** NOTE: Function blocks affect both var and let since the variable declared inside them is local and only remains as long as it is inside the function.
