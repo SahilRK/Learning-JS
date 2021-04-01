46939bfeaf8040469166c9eb33e45d493b25ad8d
Sahil R Kulkarni <sahil.kulkarni@quess.com>
2021-04-01 23:44:26 +0530

    Added code for functions in Arrays

--- MDN JS/DS.js
+++ MDN JS/DS.js
@@ -61,12 +61,87 @@ let fruits = new Array('Apple','Orange','Banana');
 let fruit_idx = fruits.indexOf("Orange");
 console.log(`Index of Orange is ${fruit_idx}`);
 
-//Array elements are object properties, but trying to access an element o an array as follows throws a syntax error because the property name is not valid.
-console.log(fruits.0) // a syntax error
+//Return array element at index location - EXPERIMENTAL
+let fruits = new Array('Apple','Orange','Banana');
+let fruit_idx = 2;
+console.log(`Element at ${fruit_idx} is : ${fruits.at(fruit_idx)}`);
+
+//Array elements are object properties, but trying to access an element 0 an array as follows throws a syntax error because the property name is not valid.
+console.log(fruits.0) //a syntax error
 //There is nothing special about this. Javascript properties that begin with a digit must be accessed using a bracket notation.
 
 //To increase the length of the array
 fruits.length = 20;
 
 //Creating an array from an array like or iteratable object using Array.from() static method
-console.log(Array.from('foo'));
+    console.log(Array.from('foo'));
+    //Array(3) ["f","o","o"]
+
+    console.log(Array.from([1,2,3],x=> x+1));
+    //Array(3) [2,3,4]
+
+//Concatenate an array is a method to join two arrays
+const arr1 = [1,2,3];
+const arr2 = [5,6,7];
+const arr_concat = arr1.concat(arr2);
+console.log(arr_concat);
+//[1,2,3,5,6,7]
+
+//Every is an array instance method that tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value
+const arr = [2,5,8,67,22];
+console.log(arr.every(function(e){
+  return e > 1
+}))
+//true
+//The same output using arrow functions would be written as so
+console.log(arr.every(e => e>1));
+
+//Filter the array and get a new array out of the filtered values
+const words = ['spray', 'limit', 'elite', 'exuberant', 'strike', 'present'];
+const filt_words = words.filter(word => word.startsWith('s'));
+console.log(filt_words);
+//Array [ "spray", "strike" ]
+
+let arr = [
+  { id: 15 },
+  { id: -1 },
+  { id: 0 },
+  { id: 3 },
+  { id: 12.2 },
+  { },
+  { id: null },
+  { id: NaN },
+  { id: 'undefined' }
+]
+
+let invalid_enteries = 0;
+
+function filterNum(item){
+  if(Number.isFinite(item.id) && item.id !== 0){
+    return true;
+  }
+  invalid_enteries++;
+  return false;
+}
+
+const filtArr = arr.filter(filterNum);
+
+console.log(filtArr);
+
+//Find return the first matching element in the array and exits
+let fruits = [
+  {name: 'banana'},
+  {name: 'apple'},
+  {name: 'papaya'}
+]
+
+console.log(fruits.find(fruit => fruit.name.includes('ap')));
+//Object { name: "apple" }
+
+
+
+
+
+
+
+
